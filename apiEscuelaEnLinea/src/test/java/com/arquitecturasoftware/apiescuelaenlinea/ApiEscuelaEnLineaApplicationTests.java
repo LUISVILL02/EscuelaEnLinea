4a6d8e347f577e85086667bdf6185054ea095cd0
luisvill02 <lvillamil@unimagdalena.edu.co>
2024-05-04 19:31:49 -0500

    first commit

---
+++ apiEscuelaEnLinea/src/test/java/com/arquitecturasoftware/apiescuelaenlinea/ApiEscuelaEnLineaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitecturasoftware.apiescuelaenlinea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEscuelaEnLineaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
