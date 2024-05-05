c7b0e957029dd0561aee6f26ba981e5957c219e9
luisvill02 <lvillamil@unimagdalena.edu.co>
2024-05-05 10:09:09 -0500

    adding the missing models from the database

---
+++ apiEscuelaEnLinea/src/main/java/com/arquitecturasoftware/apiescuelaenlinea/model/entities/Asistencia.java
@@ -0,0 +1,36 @@
+package com.arquitecturasoftware.apiescuelaenlinea.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "asistencias")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Asistencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAsistencia;
+    private LocalDate fecha;
+
+    //relationship
+    //id_estado_asisten
+
+    //alumno
+    @ManyToOne
+    @JoinColumn(name = "id_alumno")
+    private Alumno alumno;
+
+    //estadoAsistencia
+    @ManyToOne
+    @JoinColumn(name = "id_estado_asistencia")
+    private EstadoAsistencia estadoAsistencia;
+
+}
