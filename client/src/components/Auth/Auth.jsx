5ccd5ec8b48cc15c17c495562371de1a2f03c4d9
luisvill02 <lvillamil@unimagdalena.edu.co>
2024-05-08 12:06:25 -0500

    change the login styles

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -10,39 +10,44 @@ import {
 
 const Auth = () => {
   return (
-    <Flex minHeight="100vh" align="center" justify="center" bg="gray.100">
+    <Flex height={{xl: 880}} align="center"  bg="gray.100" display="flex">
       <Box
         bg="white"
-        p={8}
-        mx={{ base: 6, md: 0 }}
-        borderRadius={8}
-        boxShadow="lg"
-        display={{ base: "block", md: "flex" }}
+        height="100%"
+        width="100vw"
+        display="flex"
         alignItems="center"
+        justifyContent="center"
       >
         {/* Formulario de inicio de sesión */}
-        <Box mb={{ base: 6, md: 0 }}>
-          <Heading size="xl" mb={6}>
-            Iniciar Sesión
+        <Box width="50%" height="83%" padding=" 0 100px" display="flex" flexDirection="column">          
+          <Heading fontSize={{xl: 64, lg: 50, sm: 40, base: 28}} fontWeight="300" color="#4D44B5" mb={20}>
+            Ingreso de sesión
           </Heading>
-          <Box mr={{ base: 0, md: 10 }}>
-            <Input mb={4} placeholder="Correo electrónico" />
-            <Input mb={6} placeholder="Contraseña" type="password" />
-            <Button colorScheme="blue" mb={4}>
+          <Box mb={10}>
+            <Text fontSize="36px" color="#3D3D3D" fontWeight="300">Identificación</Text>
+            <Input placeholder="123456" height="65px" borderRadius="5px" color="#3D3D3D" fontSize="24px"/>
+          </Box>
+          <Box mb={10}>
+            <Text fontSize="36px" color="#3D3D3D" fontWeight="300">Contraseña</Text>
+            <Input placeholder="ingrese una contraseña" type="password" height="65px" borderRadius="5px" color="#3D3D3D" fontSize="24px"/>
+          </Box>
+          <Box>
+            <Button colorScheme="blue" mb={4} height="65px" width="100%" backgroundColor="#4D44B5" fontSize="32px" fontWeight="300" _hover={{bg: "white", color: "#4D44B5", border: "1px solid #4D44B5"}}>
               Iniciar Sesión
             </Button>
             <Text fontSize="sm">¿Olvidaste tu contraseña?</Text>
-          </Box>
+          </Box>          
         </Box>
 
         {/* Imagen */}
-        <Box display={{ base: "none", md: "block" }}>
+        <Box display={{ base: "none", md: "block" }}  height="100%" width="50%">
           <Image
             src="https://images.pexels.com/photos/2675061/pexels-photo-2675061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
             alt="Imagen"
-            boxSize="300px"
+            height="100%"
+            width="100%"
             objectFit="cover"
-            borderRadius={8}
           />
         </Box>
       </Box>
