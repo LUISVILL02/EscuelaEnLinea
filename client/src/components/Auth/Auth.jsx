33e9ebcbf9615832466fb6d30c70bcb36fdb3f69
juan7diaz <juanluisdiazguerrero@gmail.com>
2024-05-09 19:27:48 -0500

    feat: add the responsive login form

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,58 +1,62 @@
 import {
-  Box,
+  Button,
   Flex,
+  FormControl,
+  FormLabel,
   Heading,
   Input,
+  Link,
+  Stack,
   Image,
-  Button,
-  Text,
 } from "@chakra-ui/react";
 
-const Auth = () => {
+export default function Auth() {
   return (
-    <Flex height={{xl: 880}} align="center"  bg="gray.100" display="flex">
-      <Box
-        bg="white"
-        height="100%"
-        width="100vw"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        {/* Formulario de inicio de sesión */}
-        <Box width="50%" height="83%" padding=" 0 100px" display="flex" flexDirection="column">          
-          <Heading fontSize={{xl: 64, lg: 50, sm: 40, base: 28}} fontWeight="300" color="#4D44B5" mb={20}>
-            Ingreso de sesión
-          </Heading>
-          <Box mb={10}>
-            <Text fontSize="36px" color="#3D3D3D" fontWeight="300">Identificación</Text>
-            <Input placeholder="123456" height="65px" borderRadius="5px" color="#3D3D3D" fontSize="24px"/>
-          </Box>
-          <Box mb={10}>
-            <Text fontSize="36px" color="#3D3D3D" fontWeight="300">Contraseña</Text>
-            <Input placeholder="ingrese una contraseña" type="password" height="65px" borderRadius="5px" color="#3D3D3D" fontSize="24px"/>
-          </Box>
-          <Box>
-            <Button colorScheme="blue" mb={4} height="65px" width="100%" backgroundColor="#4D44B5" fontSize="32px" fontWeight="300" _hover={{bg: "white", color: "#4D44B5", border: "1px solid #4D44B5"}}>
-              Iniciar Sesión
+    <Stack minH={"80vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={4} w={"full"} maxW={"md"}>
+          <Heading fontSize={"2xl"}>Bienvenido de vuelta!</Heading>
+          <FormControl id="email">
+            <FormLabel>Correo Electronico</FormLabel>
+            <Input placeholder="luis@gmail.com" />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Contraseña</FormLabel>
+            <Input placeholder="****" type="password" />
+          </FormControl>
+          <Stack spacing={6}>
+            <Stack
+              direction={{ base: "column", sm: "row" }}
+              align={"start"}
+              justify={"space-between"}
+            >
+              <Link color={"#4D44B5"}>Recuperar contraseña</Link>
+            </Stack>
+            <Button
+              bg={"#4D44B5"}
+              color={"#fff"}
+              variant={"solid"}
+              _hover={{
+                bg: "#fff",
+                color: "#4D44B5",
+                border: "1px solid #4D44B5",
+              }}
+            >
+              Iniciar sesión
             </Button>
-            <Text fontSize="sm">¿Olvidaste tu contraseña?</Text>
-          </Box>          
-        </Box>
-
-        {/* Imagen */}
-        <Box display={{ base: "none", md: "block" }}  height="100%" width="50%">
-          <Image
-            src="https://images.pexels.com/photos/2675061/pexels-photo-2675061.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt="Imagen"
-            height="100%"
-            width="100%"
-            objectFit="cover"
-          />
-        </Box>
-      </Box>
-    </Flex>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          display={{ base: "none", md: "block" }}
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={
+            "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
+          }
+        />
+      </Flex>
+    </Stack>
   );
-};
-
-export default Auth;
+}
