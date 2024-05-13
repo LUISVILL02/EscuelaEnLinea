02e10b4e87ad52c23a0ce95c982fd2b0d4bd15f0
juan7diaz <juanluisdiazguerrero@gmail.com>
2024-05-13 00:47:01 -0500

    refactor: add the custom hook useLogIn

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -13,40 +13,18 @@ import {
   AlertTitle,
   AlertDescription,
 } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
-import { users, useUserStore } from "../../store";
-import { useNavigate } from "react-router-dom";
-import { routes } from "../../config";
+
+import { useLogIn } from "../../hooks";
 
 export default function Auth() {
   const {
     handleSubmit,
-    formState: { errors, isSubmitting, isValid },
+    errors,
+    isSubmitting,
+    isValid,
     register,
-    setError,
-  } = useForm();
-
-  const logIn = useUserStore((state) => state.logIn);
-
-  const navigate = useNavigate();
-
-  const onSubmitHandler = async (data) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const userFound = users.find((user) => user.email === data.email);
-        if (userFound) {
-          logIn(userFound);
-          navigate(routes.AUTH); // esto deberia redirigir a la ruta de la app, (aun no existe por lo que redirige a la ruta de atenticacion)
-        } else {
-          setError("email", {
-            type: "manual",
-            message: "correo o contraseña incorrectos",
-          });
-        }
-        resolve();
-      }, 1500);
-    });
-  };
+    onSubmitHandler,
+  } = useLogIn();
 
   return (
     <Stack
---
+++ client/src/hooks/useLogIn.js
@@ -0,0 +1,46 @@
+import { useForm } from "react-hook-form";
+import { useUserStore, users } from "../store";
+import { routes } from "../config";
+import { useNavigate } from "react-router-dom";
+
+const useLogIn = () => {
+  const {
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid },
+    register,
+    setError,
+  } = useForm();
+
+  const logIn = useUserStore((state) => state.logIn);
+
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (data) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const userFound = users.find((user) => user.email === data.email);
+        if (userFound) {
+          logIn(userFound);
+          navigate(routes.AUTH); // esto deberia redirigir a la ruta de la app, (aun no existe por lo que redirige a la ruta de atenticacion)
+        } else {
+          setError("email", {
+            type: "manual",
+            message: "correo o contraseña incorrectos",
+          });
+        }
+        resolve();
+      }, 1500);
+    });
+  };
+
+  return {
+    handleSubmit,
+    errors,
+    isSubmitting,
+    isValid,
+    register,
+    onSubmitHandler,
+  };
+};
+
+export default useLogIn;
