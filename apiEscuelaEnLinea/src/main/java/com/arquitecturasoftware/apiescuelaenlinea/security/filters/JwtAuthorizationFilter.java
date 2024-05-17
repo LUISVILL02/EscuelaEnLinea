f54461c46a31bfd775a5b0fc8fdec0195cb00ab0
luisvill02 <lvillamil@unimagdalena.edu.co>
2024-05-16 22:45:03 -0500

    Error detected when injecting JwtUtil bean in JwtAuthorizationFilter

--- apiEscuelaEnLinea/src/main/java/com/arquitecturasoftware/apiescuelaenlinea/security/filters/JwtAuthorizationFilter.java
+++ apiEscuelaEnLinea/src/main/java/com/arquitecturasoftware/apiescuelaenlinea/security/filters/JwtAuthorizationFilter.java
@@ -6,8 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,11 +20,14 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-@AllArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
-    private final JwtUtils jwtUtils;
-    private final UserDetailsService userDetailsService;
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String token = getTokenFromRequest(request);
--- apiEscuelaEnLinea/src/main/java/com/arquitecturasoftware/apiescuelaenlinea/security/jwt/JwtUtils.java
+++ apiEscuelaEnLinea/src/main/java/com/arquitecturasoftware/apiescuelaenlinea/security/jwt/JwtUtils.java
@@ -7,12 +7,14 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.function.Function;
+
 
 @Service
 public class JwtUtils {
