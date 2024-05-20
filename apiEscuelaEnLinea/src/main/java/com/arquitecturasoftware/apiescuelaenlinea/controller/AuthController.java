b43095337b7782ffa2f8b364ad1398f299f94182
luisvill02 <lvillamil@unimagdalena.edu.co>
2024-05-20 12:03:00 -0500

    Change

--- apiEscuelaEnLinea/src/main/java/com/arquitecturasoftware/apiescuelaenlinea/controller/AuthController.java
+++ apiEscuelaEnLinea/src/main/java/com/arquitecturasoftware/apiescuelaenlinea/controller/AuthController.java
@@ -7,7 +7,6 @@ import com.arquitecturasoftware.apiescuelaenlinea.model.dtosGuardar.Administrado
 import com.arquitecturasoftware.apiescuelaenlinea.model.dtosGuardar.ProfesorGDto;
 import com.arquitecturasoftware.apiescuelaenlinea.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -19,7 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Autenticación", description = "Endpioint de autenticación y registro de usuarios")
+@Tag(name = "Autenticación", description = "Recursos de autenticación y registro de usuarios")
 @RestController
 @RequestMapping("/EscuelaEnLinea/V.1.0.0/auth")
 @AllArgsConstructor
@@ -30,6 +29,14 @@ public class AuthController {
             summary = "Inicio de sesión",
             description = "Iniciar sesion con correo y contraseña",
             tags = { "Login", "Post" })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Login.class),
+                    examples = @ExampleObject(name = "Ejemplo de inicio de sesión",
+                                    value = "{\n\"correo\": \"villamilluis123@gmail.com\", \n\"contraseña\": \"password123\"\n}" ,
+                                    description = "Correo del usuario")
+    ))
     @ApiResponses({
             @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = ResponseJwt.class), mediaType = "application/json") }),
             @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema(implementation = String.class), mediaType = "application/json") })})
@@ -50,6 +57,7 @@ public class AuthController {
             @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = String.class), mediaType = "application/json") }),
             @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema(implementation = String.class), mediaType = "application/json") })})
     @PostMapping("/register/acudiente")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<String> registerAcudiente(@RequestBody AcudienteGDto acudienteGDto) {
         try {
             return ResponseEntity.ok(authService.registerAcudiente(acudienteGDto));
@@ -62,10 +70,31 @@ public class AuthController {
             summary = "Registrar profesor",
             description = "Registrar un profesor en la base de datos",
             tags = { "Signup", "Post" })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProfesorGDto.class),
+                    examples = @ExampleObject(name = "Ejemplo de inicio de sesión",
+                            value = "{\n" +
+                                    "  \"nombre\": \"Juan\",\n" +
+                                    "  \"apellido\": \"Pérez\",\n" +
+                                    "  \"identificacion\": \"12345678\",\n" +
+                                    "  \"correo\": \"usuario@ejemplo.com\",\n" +
+                                    "  \"telefono\": \"34123456789\",\n" +
+                                    "  \"contraseña\": \"password123\",\n" +
+                                    "  \"roles\": [\n" +
+                                    "    \"PROFESOR\"\n" +
+                                    "  ],\n" +
+                                    "  \"fechaNacimiento\": \"15/09/2002\",\n" +
+                                    "  \"direccion\": \"Calle Falsa 123\"\n" +
+                                    "}",
+                            description = "Json con los datos del profesor a guardar")
+            ))
     @ApiResponses({
             @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = String.class), mediaType = "application/json") }),
             @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema(implementation = String.class), mediaType = "application/json") })})
     @PostMapping("/register/profesor")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<String> registerProfesor(@RequestBody ProfesorGDto profesorGDto) {
         try {
             return ResponseEntity.ok(authService.registerProfesor(profesorGDto));
@@ -82,17 +111,12 @@ public class AuthController {
             @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = String.class), mediaType = "application/json") }),
             @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema(implementation = String.class), mediaType = "application/json") })})
     @PostMapping("/register/administrador")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<String> registerAdministrador(@RequestBody AdministradorGDto administradorGDto) {
         try {
             return ResponseEntity.ok(authService.registerAdministrador(administradorGDto));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
-    }
-
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @GetMapping("/test")
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok("Prueba de autorización con token válido");
     }
 }
