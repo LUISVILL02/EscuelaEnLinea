5bd43dcb54fbc8a7c06f17922b20e8d4ce99e85a
LUISVILL <125088411+LUISVILL02@users.noreply.github.com>
2024-09-07 14:40:16 -0500

    Update WebConfig.java

--- apiEscuelaEnLinea/src/main/java/com/arquitecturasoftware/apiescuelaenlinea/security/config/WebConfig.java
+++ apiEscuelaEnLinea/src/main/java/com/arquitecturasoftware/apiescuelaenlinea/security/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://main--escuelaenlineaapp.netlify.app");
+        config.addAllowedOrigin("https://escuelaenlineaapp.netlify.app");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
